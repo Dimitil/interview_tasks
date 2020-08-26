d60b3201a8baae763ab826444021a22faa54fa05
Dimitil <boikovdmitrii@gmail.com>
2020-08-26 18:40:47 +0300

    modif isOnemodif

--- modifLenString.cpp
+++ modifLenString.cpp
@@ -2,26 +2,32 @@
 #include <string>
 
 bool isOneDelete(std::string str1, std::string str2){
-    int diffCount=0;
-    for(int firIt=0, secIt=0; firIt<str1.size(), secIt<str2.size(); firIt++, secIt++){
-        if(diffCount>1) return false;
-        else if(str1[firIt]!=str2[secIt]){
-            diffCount++;
-            firIt++;
+    int index1=0, index2=0;
+    while(index1<str1.size() && index2<str2.size())
+    {
+        if(str1[index1]!=str2[index2]){
+            if(index1!=index2) return false;
+            index1++;
         }
+        index1++;
+        index2++;
     }
-    return true;
+       return true;
 }
 
 
 bool isOneReplace(std::string str1, std::string str2){
     int diffCount=0;
-    for(int firIt=0, secIt=0; firIt<str1.size(), secIt<str2.size(); firIt++, secIt++){
-        if(diffCount>1) return false;
-        else if(str1[firIt]!=str2[secIt]) {
-            diffCount++;
-        }
-    }
+    int index1=0, index2=0;
+
+   while(index1<str1.size() && index2<str2.size()){
+       if(str1[index1]!=str2[index2]){
+           diffCount++;
+       }
+       index1++;
+       index2++;
+       if(diffCount>1) return false;
+    } 
     return true;
 }
 
@@ -30,7 +36,7 @@ bool isOneReplace(std::string str1, std::string str2){
 bool isOneModification(std::string str1, std::string str2){//1 or 0 modification
     if(str1.size()==str2.size()) return isOneReplace(str1, str2);
     else if(str1.size()+1==str2.size()) return isOneDelete(str1, str2);
-    else if(str2.size()-str1.size() > 1) return isOneDelete(str2, str1); 
+    else if(str1.size()-1==str2.size()) return isOneDelete(str2, str1); 
     else return false;
 }
 
@@ -38,8 +44,8 @@ bool isOneModification(std::string str1, std::string str2){//1 or 0 modification
 
 int main()
 {
-    std::string str1("pale");
-    std::string str2("pae");
+    std::string str1("pla");
+    std::string str2("ple");
 
     std::cout<<'\n'<<isOneModification(str1, str2)<<'\n';
 
