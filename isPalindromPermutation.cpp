0ed93ea631882270b9326301e89c1756ba9a74ee
Mit <63301421+Dimitil@users.noreply.github.com>
2020-08-27 00:14:22 +0300

    Update isPalindromPermutation.cpp

--- isPalindromPermutation.cpp
+++ isPalindromPermutation.cpp
@@ -6,19 +6,19 @@ bool isPaliPer(const std::string str){
     int ar[128]={0};
     int countOdd=0;
 
-for(int i=0; i<str.size(); i++){
-    int val=str[i];
-    if(val<0 || val>127) {
+    for(int i=0; i<str.size(); i++){
+        int val=str[i];
+        if(val<0 || val>127) {
             std::cerr<<"NOT ANSII SYMBOL";
             return false;
-        }
-        ar[val]++;
-        if(ar[val] % 2 == 1){
-            countOdd++;
-        }
-        else countOdd--;
-        }
-        return countOdd <= 1;
+    }
+    ar[val]++;
+    if(ar[val] % 2 == 1){
+        countOdd++;
+    }
+    else countOdd--;
+    }
+    return countOdd <= 1;
 }
 
 int main()
